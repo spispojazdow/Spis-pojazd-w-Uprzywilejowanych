a994cad7e8533b5267df5dda85b424e68310d572
spispojazdow <spispojazdow@gmail.com>
2026-06-23 20:36:47 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,173 @@
+const dane = {
+    "Kujawsko-Pomorskie": [
+        { kod: "300[C]", miasto: "Bydgoszcz" },
+        { kod: "340[C]", miasto: "Toruń" },
+        { kod: "360[C]", miasto: "Włocławek" },
+        { kod: "400[C]", miasto: "Aleksandrów Kujawski" },
+        { kod: "410[C]", miasto: "Brodnica" },
+        { kod: "420[C]", miasto: "Chełmno" },
+        { kod: "430[C]", miasto: "Golub-Dobrzyń" },
+        { kod: "440[C]", miasto: "Grudziądz" },
+        { kod: "460[C]", miasto: "Inowrocław" },
+        { kod: "470[C]", miasto: "Liman" },
+        { kod: "480[C]", miasto: "Mogilno" },
+        { kod: "490[C]", miasto: "Nakło nad Notecią" },
+        { kod: "500[C]", miasto: "Radziejów" },
+        { kod: "510[C]", miasto: "Rypin" },
+        { kod: "520[C]", miasto: "Sępólno Krajeńskie" },
+        { kod: "530[C]", miasto: "Świecie" },
+        { kod: "540[C]", miasto: "Tuchola" },
+        { kod: "550[C]", miasto: "Wąbrzeźno" },
+        { kod: "560[C]", miasto: "Żnin" }
+    ],
+    "Dolnośląskie": [
+        { kod: "200[C]", miasto: "Wrocław" },
+        { kod: "210[C]", miasto: "Bolesławiec" },
+        { kod: "220[C]", miasto: "Dzierżoniów" },
+        { kod: "230[C]", miasto: "Głogów" },
+        { kod: "240[C]", miasto: "Jawor" },
+        { kod: "250[C]", miasto: "Jelenia Góra" },
+        { kod: "260[C]", miasto: "Kamienna Góra" },
+        { kod: "270[C]", miasto: "Kłodzko" },
+        { kod: "280[C]", miasto: "Legnica" },
+        { kod: "290[C]", miasto: "Lubań" },
+        { kod: "300[C]", miasto: "Lubin" },
+        { kod: "310[C]", miasto: "Oława" },
+        { kod: "320[C]", miasto: "Oleśnica" },
+        { kod: "330[C]", miasto: "Polkowice" },
+        { kod: "340[C]", miasto: "Świdnica" },
+        { kod: "350[C]", miasto: "Trzebnica" },
+        { kod: "360[C]", miasto: "Wałbrzych" },
+        { kod: "370[C]", miasto: "Wołów" },
+        { kod: "380[C]", miasto: "Zgorzelec" },
+        { kod: "390[C]", miasto: "Złotoryja" }
+    ],
+    "Mazowieckie": [
+        { kod: "100[C]", miasto: "Warszawa" },
+        { kod: "110[C]", miasto: "Radom" },
+        { kod: "120[C]", miasto: "Płock" },
+        { kod: "130[C]", miasto: "Siedlce" },
+        { kod: "140[C]", miasto: "Ostrołęka" },
+        { kod: "150[C]", miasto: "Ciechanów" },
+        { kod: "160[C]", miasto: "Gostynin" },
+        { kod: "170[C]", miasto: "Grójec" },
+        { kod: "180[C]", miasto: "Legionowo" },
+        { kod: "190[C]", miasto: "Mińsk Mazowiecki" },
+        { kod: "200[C]", miasto: "Nowy Dwór Mazowiecki" },
+        { kod: "210[C]", miasto: "Otwock" },
+        { kod: "220[C]", miasto: "Piaseczno" },
+        { kod: "230[C]", miasto: "Pruszków" },
+        { kod: "240[C]", miasto: "Pułtusk" },
+        { kod: "250[C]", miasto: "Sochaczew" },
+        { kod: "260[C]", miasto: "Wyszków" },
+        { kod: "270[C]", miasto: "Zwoleń" }
+    ],
+    "Pomorskie": [
+        { kod: "600[C]", miasto: "Gdańsk" },
+        { kod: "610[C]", miasto: "Gdynia" },
+        { kod: "620[C]", miasto: "Sopot" },
+        { kod: "630[C]", miasto: "Słupsk" },
+        { kod: "640[C]", miasto: "Bytów" },
+        { kod: "650[C]", miasto: "Chojnice" },
+        { kod: "660[C]", miasto: "Człuchów" },
+        { kod: "670[C]", miasto: "Kartuzy" },
+        { kod: "680[C]", miasto: "Kościerzyna" },
+        { kod: "690[C]", miasto: "Kwidzyn" },
+        { kod: "700[C]", miasto: "Malbork" },
+        { kod: "710[C]", miasto: "Nowy Dwór Gdański" },
+        { kod: "720[C]", miasto: "Puck" },
+        { kod: "730[C]", miasto: "Starogard Gdański" },
+        { kod: "740[C]", miasto: "Wejherowo" }
+    ],
+    "Małopolskie": [
+        { kod: "800[C]", miasto: "Kraków" },
+        { kod: "810[C]", miasto: "Tarnów" },
+        { kod: "820[C]", miasto: "Nowy Sącz" },
+        { kod: "830[C]", miasto: "Bochnia" },
+        { kod: "840[C]", miasto: "Brzesko" },
+        { kod: "850[C]", miasto: "Chrzanów" },
+        { kod: "860[C]", miasto: "Dąbrowa Tarnowska" },
+        { kod: "870[C]", miasto: "Gorlice" },
+        { kod: "880[C]", miasto: "Limanowa" },
+        { kod: "890[C]", miasto: "Miechów" },
+        { kod: "900[C]", miasto: "Myślenice" },
+        { kod: "910[C]", miasto: "Nowy Targ" },
+        { kod: "920[C]", miasto: "Olkusz" },
+        { kod: "930[C]", miasto: "Oświęcim" },
+        { kod: "940[C]", miasto: "Proszowice" },
+        { kod: "950[C]", miasto: "Sucha Beskidzka" },
+        { kod: "960[C]", miasto: "Wieliczka" },
+        { kod: "970[C]", miasto: "Zakopane" }
+    ],
+    "Śląskie": [
+        { kod: "500[C]", miasto: "Katowice" },
+        { kod: "510[C]", miasto: "Częstochowa" },
+        { kod: "520[C]", miasto: "Sosnowiec" },
+        { kod: "530[C]", miasto: "Gliwice" },
+        { kod: "540[C]", miasto: "Zabrze" },
+        { kod: "550[C]", miasto: "Bytom" },
+        { kod: "560[C]", miasto: "Ruda Śląska" },
+        { kod: "570[C]", miasto: "Tychy" },
+        { kod: "580[C]", miasto: "Dąbrowa Górnicza" },
+        { kod: "590[C]", miasto: "Chorzów" },
+        { kod: "600[C]", miasto: "Jaworzno" },
+        { kod: "610[C]", miasto: "Mysłowice" },
+        { kod: "620[C]", miasto: "Siemianowice Śląskie" },
+        { kod: "630[C]", miasto: "Będzin" },
+        { kod: "640[C]", miasto: "Piekary Śląskie" },
+        { kod: "650[C]", miasto: "Racibórz" },
+        { kod: "660[C]", miasto: "Rydułtowy" },
+        { kod: "670[C]", miasto: "Tarnowskie Góry" },
+        { kod: "680[C]", miasto: "Wodzisław Śląski" },
+        { kod: "690[C]", miasto: "Żory" }
+    ],
+    "Wielkopolskie": [
+        { kod: "700[C]", miasto: "Poznań" },
+        { kod: "710[C]", miasto: "Kalisz" },
+        { kod: "720[C]", miasto: "Konin" },
+        { kod: "730[C]", miasto: "Piła" },
+        { kod: "740[C]", miasto: "Leszno" },
+        { kod: "750[C]", miasto: "Chodzież" },
+        { kod: "760[C]", miasto: "Czarnków" },
+        { kod: "770[C]", miasto: "Gniezno" },
+        { kod: "780[C]", miasto: "Gostyń" },
+        { kod: "790[C]", miasto: "Jarocin" },
+        { kod: "800[C]", miasto: "Kępno" },
+        { kod: "810[C]", miasto: "Koło" },
+        { kod: "820[C]", miasto: "Kościan" },
+        { kod: "830[C]", miasto: "Krotoszyn" },
+        { kod: "840[C]", miasto: "Międzychód" },
+        { kod: "850[C]", miasto: "Nowy Tomyśl" },
+        { kod: "860[C]", miasto: "Oborniki" },
+        { kod: "870[C]", miasto: "Ostrów Wielkopolski" },
+        { kod: "880[C]", miasto: "Ostrzeszów" },
+        { kod: "890[C]", miasto: "Pleszew" },
+        { kod: "900[C]", miasto: "Rawicz" },
+        { kod: "910[C]", miasto: "Słupca" },
+        { kod: "920[C]", miasto: "Szamotuły" },
+        { kod: "930[C]", miasto: "Śrem" },
+        { kod: "940[C]", miasto: "Środa Wielkopolska" },
+        { kod: "950[C]", miasto: "Turek" },
+        { kod: "960[C]", miasto: "Wągrowiec" },
+        { kod: "970[C]", miasto: "Wolsztyn" },
+        { kod: "980[C]", miasto: "Września" },
+        { kod: "990[C]", miasto: "Złotów" }
+    ]
+};
+
+document.getElementById('wojewodztwo').addEventListener('change', function() {
+    const wybrane = this.value;
+    const lista = document.getElementById('wyniki');
+    const naglowek = document.getElementById('naglowek-lista');
+
+    if (!wybrane || !dane[wybrane]) {
+        lista.innerHTML = '';
+        naglowek.textContent = 'Wybierz województwo';
+        return;
+    }
+
+    naglowek.textContent = wybrane;
+    lista.innerHTML = dane[wybrane]
+        .map(item => `<li><span class="kod">${item.kod}</span><span class="miasto">${item.miasto}</span></li>`)
+        .join('');
+});
